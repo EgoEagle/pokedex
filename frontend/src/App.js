183d51c96dd1ab1566ef3a1ab198886cfc470107
Tony <TonyLin3591@gmail.com>
2022-07-25 15:49:07 -0400

    deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
 import Display from './components/Display.js';
 import Dex from './components/Dex.js';
-import React , {useState} from 'react';
+import React , {useState , useEffect} from 'react';
+import axios from 'axios'
 
 
 function App() {
-  const[pokemon, setPokemon] = useState(["charmander","bulbasaur"]);
+  const[pokemon, setPokemon] = useState([]);
+  const[currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const[nextPageUrl, setNextPageUrl] = useState()
+  const[prevPageUrl, setPrevPageUrl] = useState()
+  const[loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
+      setLoading(false)
+      setPokemon(res.data.results.map(p => p.name))
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.prev);
+  })
+
+},[currentPageUrl])
+
 
     return(
       <div>
